da4fdf4a89e1089e2d643684f64e87409dc8e835
Mayur <mayur@kagrana.com>
2014-11-14 12:58:49 +0530

    Changing log file. Adding reporting.

--- src/main/java/com/kagrana/util/Log.java
+++ src/main/java/com/kagrana/util/Log.java
@@ -1,17 +1,56 @@
 package com.kagrana.util;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+import com.kagrana.DTO.TestCase;
+import com.kagrana.DTO.TestSuite;
+import com.kagrana.DTO.TestSuiteConfig;
 import com.kagrana.reporting.ReportingType;
 
 public class Log {
 	private boolean debugEnabled = true;
 	private ReportingType reportingType;
+	private static TestSuite suite;
+	private String reportDirectory;
 	public Log(){
-		
+		suite = new TestSuite();
+		reportingType = ReportingType.HTML;
+	}
+	/**
+	 * This function helps setup test suite configuration.
+	 * @param testSuiteConfig
+	 */
+	public void setTestSuiteConfig(TestSuiteConfig testSuiteConfig){
+		suite.setTestSuiteName(testSuiteConfig.getTestSuiteName());
+		suite.setEnvironment(testSuiteConfig.getEnvironment());
+		suite.setExecutionType(testSuiteConfig.getExecutionType());
+		suite.setTestDateTime(testSuiteConfig.getTestDateTime());
+	}
+	public String getReportDirectory() {
+		return reportDirectory;
+	}
+	/**
+	 * Set up report directory. Default is report;
+	 * @param reportDirectory
+	 */
+	public void setReportDirectory(String reportDirectory){
+		this.reportDirectory = reportDirectory;
+	}
+	/**
+	 * This function will add TestCase to TestSuite
+	 * @param testCase
+	 */
+	public void addTestCase(TestCase testCase){
+		suite.addTestCases(testCase);
 	}
 	public Log(ReportingType reportingType){
+		this();
 		this.reportingType = reportingType;
 	}
-
+	
 	public boolean isDebugEnabled() {
 		return debugEnabled;
 	}
@@ -34,5 +73,44 @@ public class Log {
 		if(isDebugEnabled())
 			System.err.println(message);
 	}
+	public void writeReport() throws IOException{
+		if(getReportDirectory() == null){
+			File currentDir = new File(".");
+			File reportDir = new File(currentDir.getAbsolutePath() + File.pathSeparator + "reports"+File.separator);
+			if(reportDir.exists()){
+					FileUtils.cleanDirectory(reportDir);
+					reportDir.delete();
+					if(reportDir.mkdir())
+						this.write("Report directory created!");
+					else
+						this.writeError("Error creating report directory");
+				}
+			else{
+				if(reportDir.mkdir())
+					this.write("Report directory created!");
+				else
+					this.writeError("Error creating report directory");
+			}
+			this.setReportDirectory(reportDir.getAbsolutePath());
+		}
+		if(reportingType.equals(ReportingType.HTML))
+			writeHTMLReport(getReportDirectory());
+		else
+			writeExcelReport(getReportDirectory());
+	}
+	/**
+	 * Generate report in Excel file.
+	 */
+	private void writeExcelReport(String reportDirectory){
+		
+		
+	}
+	/**
+	 * Generate report in HTML file
+	 */
+	private void writeHTMLReport(String reportDirectory){
+		
+	}
+
 
 }
