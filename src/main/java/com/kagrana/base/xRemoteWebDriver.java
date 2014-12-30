6e9f18a123662efe5e5ea695c4ac6009b9df8a17
Mayur <mayur@kagrana.com>
2014-12-30 21:06:30 +0530

    Removed log from xRemoteWebDriver

--- src/main/java/com/kagrana/base/xRemoteWebDriver.java
+++ src/main/java/com/kagrana/base/xRemoteWebDriver.java
@@ -4,44 +4,35 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Map;
-
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.Command;
 import org.openqa.selenium.remote.CommandExecutor;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.Response;
-
 import com.kagrana.DTO.WebDriverConfig;
 import com.kagrana.util.Log;
 
 
 
 public class xRemoteWebDriver extends RemoteWebDriver {
-	private Log log;
 		
 	  
 	  public xRemoteWebDriver(CommandExecutor executor, Capabilities desiredCapabilities,Log log)
 	  {
 	    super(executor, desiredCapabilities);
-	    this.log = log;
 	  }
 	  
 	  public xRemoteWebDriver(Capabilities desiredCapabilities,Log log)
 	  {
 	    super(desiredCapabilities);
-	    this.log = log;
 	  }
 	  
 	  public xRemoteWebDriver(URL remoteAddress, Capabilities desiredCapabilities, Log log)
 	  {
 	    super(remoteAddress, desiredCapabilities);
-	    this.log = log;
 	  }
 	  public static WebDriver getInstance(WebDriverConfig config,Log log) throws MalformedURLException, FileNotFoundException{
 		  if(!config.isIntenal()){
@@ -71,13 +62,4 @@ public class xRemoteWebDriver extends RemoteWebDriver {
 		  return new FirefoxDriver();
 		  
 	  }
-	  protected Response execute(String driverCommand, Map<String, ?> parameters)
-	  {
-	    Response response = super.execute(driverCommand, parameters);
-	    Command command = new Command(super.getSessionId(), driverCommand, 
-	      parameters);
-	    log.write("Executing: " + command.toString());
-	    return response;
-	  }
-
 }
